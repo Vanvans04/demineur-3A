d82bbea986532a559b024813585bd73aa1129dca
Vanvans04 <ivanntherin04@gmail.com>
2026-03-29 13:00:08 +0200

    Finition jeu hexagonal et début modification fenêtre

--- src/GrilleH.java
+++ src/GrilleH.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Random;
 
 public class GrilleH extends Grille{
     public GrilleH(int taille){
@@ -15,14 +14,10 @@ public class GrilleH extends Grille{
         ArrayList<Case> voisins = new ArrayList<>();
 
         if(posCase[1] % 2 == 0){
-            // Colonnes PAIRES (Regardent vers le haut)
-            // Ordre : Haut, Bas, Haut-Gauche, Bas-Gauche, Haut-Droit, Bas-Droit
             positionsI = new int[]{ -1, 1, -1,  0, -1,  0 };
             positionsJ = new int[]{  0, 0, -1, -1,  1,  1 };
         }
         else{
-            // Colonnes IMPAIRES (Regardent vers le bas)
-            // Ordre : Haut, Bas, Haut-Gauche, Bas-Gauche, Haut-Droit, Bas-Droit
             positionsI = new int[]{ -1, 1,  0,  1,  0,  1 };
             positionsJ = new int[]{  0, 0, -1, -1,  1,  1 };
         }
